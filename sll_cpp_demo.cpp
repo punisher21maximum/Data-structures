ea52a3db89ba9f713eb5af83ee90c0b9150c4360
Vishal Singh <effort21pool@gmail.com>
2020-05-23 13:57:51 +0530

    just node

---
+++ sll_cpp_demo.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+class Node{
+	public:
+		int data;
+		Node * next;
+};
+
+int main()
+{
+	Node * head = NULL;
+	Node * second = NULL;
+	Node * third = NULL;
+	
+	head = new Node();
+	second = new Node();
+	third = new Node();
+	
+	head->data = 1; head->next=NULL;
+	second->data = 2; second->next=NULL;
+	third->data=3; third->next=NULL;
+		
+	
+	return 0;
+}
